f6855715b96f92d0dbf58ae98e6211b75d82dd6a
tedkuo1268 <51361941+tedkuo1268@users.noreply.github.com>
2019-09-17 02:59:04 -0500

    Update main.py

--- Project1_Finding_Lane_Lines/main.py
+++ Project1_Finding_Lane_Lines/main.py
@@ -7,11 +7,6 @@ if __name__ == "__main__":
     
     # Output a video
     white_output = 'test_videos_output/solidWhiteRight.mp4'
-    ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
-    ## To do so add .subclip(start_second,end_second) to the end of the line below
-    ## Where start_second and end_second are integer values representing the start and end of the subclip
-    ## You may also uncomment the following line for a subclip of the first 5 seconds
-    ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
     clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4")
     white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
     white_clip.write_videofile(white_output, audio=False)
