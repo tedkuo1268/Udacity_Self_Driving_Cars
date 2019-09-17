ff13f56df710735e8e7cd85d46b94b9c3034c356
tedkuo1268 <51361941+tedkuo1268@users.noreply.github.com>
2019-09-17 03:00:19 -0500

    Update find_lane.py

--- Project1_Finding_Lane_Lines/find_lane.py
+++ Project1_Finding_Lane_Lines/find_lane.py
@@ -47,24 +47,7 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-# MODIFIED (two input arguments are added to specify the upper and lower values of the line in y-direction)
 def draw_lines(img, lines, y_upper, y_bottom, color=[255, 0, 0], thickness=6):
-    """
-        NOTE: this is the function you might want to use as a starting point once you want to
-        average/extrapolate the line segments you detect to map out the full
-        extent of the lane (going from the result shown in raw-lines-example.mp4
-        to that shown in P1_example.mp4).
-        
-        Think about things like separating line segments by their
-        slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-        line vs. the right line.  Then, you can average the position of each of
-        the lines and extrapolate to the top and bottom of the lane.
-        
-        This function draws `lines` with `color` and `thickness`.
-        Lines are drawn on the image inplace (mutates the image).
-        If you want to make the lines semi-transparent, think about combining
-        this function with the weighted_img() function below
-        """
     
     x1_right_sum = 0
     x2_right_sum = 0
@@ -126,8 +109,6 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
     draw_lines(line_img, lines, math.floor(0.6 * line_img.shape[0]), line_img.shape[0])
     return line_img
-
-# Python 3 has support for cool math symbols.
 
 def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
     """
